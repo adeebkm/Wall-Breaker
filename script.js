23132e0e42b602a51dc8b5c65c5a23a1e87c1ea3
Adeeb K.M <91605126+adeebkm@users.noreply.github.com>
2022-05-08 13:04:18 +0530

    New design and game finally works

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const paddle= {
     y: canvas.height-20,
     w:80,
     h:10,
-    speeed:8,
+    speed:8,
     dx:0
 }
 
@@ -52,7 +52,7 @@ for(let i=0;i<brickRowCount;i++){
 function drawBall() {
     ctx.beginPath();
     ctx.arc(ball.x,ball.y,ball.size,0,Math.PI*2);
-    ctx.fillStyle='#0095dd';
+    ctx.fillStyle='#3EB489';
     ctx.fill();
     ctx.closePath();
 }
@@ -60,17 +60,12 @@ function drawBall() {
 function drawPaddle(){
     ctx.beginPath();
     ctx.rect(paddle.x,paddle.y,paddle.w,paddle.h);
-    ctx.fillStyle='#0095dd'
+    ctx.fillStyle='white';
     ctx.fill();
     ctx.closePath();
 }
 
-function draw() {
-    drawBall();
-    drawPaddle();
-    drawScore();
-    drawBricks();
-}
+
 
 function drawScore() {
     ctx.font = '20px Arial';
@@ -82,14 +77,104 @@ function drawBricks() {
         column.forEach(brick =>{
             ctx.beginPath();
             ctx.rect(brick.x,brick.y,brick.w,brick.h);
-            ctx.fillStyle=brick.visible ? '#0095dd': 'transparent';
+            ctx.fillStyle=brick.visible ? 'red': 'transparent';
             ctx.fill();
             ctx.closePath();
         })
     })
 }
 
-draw();
+function movePaddle() {
+    paddle.x += paddle.dx;
+    if(paddle.x + paddle.w > canvas.width){
+        paddle.x = canvas.width - paddle.w;
+    }
+
+    if(paddle.x < 0){
+        paddle.x = 0;
+    }
+}
+
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+    if(ball.x+ball.size > canvas.width || ball.x - ball.size < 0){
+        ball.dx *= -1;
+    }
+    if(ball.y + ball.size > canvas.height || ball.y - ball.size < 0){
+        ball.dy *= -1;
+    }
+
+    if(ball.x-ball.size>paddle.x && ball.x+ball.size<paddle.x+paddle.w && ball.y+ball.size>paddle.y){
+        ball.dy = -ball.speed;}
+
+        bricks.forEach(column =>{
+            column.forEach(brick =>{ 
+                if(brick.visible){
+                if(ball.x-ball.size>brick.x && ball.x+ball.size<brick.x+brick.w && ball.y+ball.size>brick.y && ball.y-ball.size<brick.y+brick.h){
+                    ball.dy *= -1;
+                    brick.visible = false;
+
+                    increaseScore();
+            }
+        }
+    });
+});
+
+    if(ball.y + ball.size > canvas.height){
+        showAllBricks();
+        score = 0;
+}
+
+}
+
+function increaseScore() {
+    score++;
+    if(score % (brickRowCount * brickRowCount) === 0){
+        showAllBricks();
+
+}
+}
+
+function showAllBricks() {
+    bricks.forEach(column =>{
+        column.forEach(brick =>(brick.visible = true));
+});
+}
+
+function draw() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    drawBall();
+    drawPaddle();
+    drawScore();
+    drawBricks();
+}
+
+function update() {
+    movePaddle();
+    moveBall();
+    draw();
+    requestAnimationFrame(update);
+}
+
+update();
+
+function keyDown(e) {
+    if(e.key === 'Right' || e.key === 'ArrowRight'){
+        paddle.dx = paddle.speed;
+    } else if(e.key === 'Left' || e.key === 'ArrowLeft'){
+        paddle.dx = -paddle.speed;
+    }
+}
+function keyUp(e) {
+    if(e.key === 'Right' || e.key === 'ArrowRight' || e.key === 'Left' || e.key === 'ArrowLeft'){
+        paddle.dx = 0;
+    }
+}
+
+document.addEventListener('keydown',keyDown);
+document.addEventListener('keyup',keyUp);
 
 rulesBtn.addEventListener('click',()=>rules.classList.add('show'));
 closeBtn.addEventListener('click',()=>rules.classList.remove('show'));
